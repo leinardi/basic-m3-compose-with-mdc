324b5c486928142eb176c27fbe9664e769a11a14
Roberto Leinardi <roberto.leinardi@veeva.com>
2023-01-12 14:31:55 +0100

    KeyboardActions error

--- app/src/main/java/com/example/basicm3compose/MainActivity.kt
+++ app/src/main/java/com/example/basicm3compose/MainActivity.kt
@@ -1,43 +1,89 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.example.basicm3compose
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.example.basicm3compose.ui.theme.BasicM3ComposeTheme
-import com.google.android.material.datepicker.MaterialDatePicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen() // ... or disable the splash and it will work
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false) // Uncomment this...
-        installSplashScreen() // ... or disable the splash and it will work
         setContent {
             BasicM3ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .systemBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
             }
         }
-
-        MaterialDatePicker.Builder.datePicker().build().show(supportFragmentManager, "DatePicker")
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+    val focusManager = LocalFocusManager.current
+    var username by remember { mutableStateOf("username") }
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester)
+            .onPreviewKeyEvent { keyEvent ->
+                (keyEvent.key == Key.Tab).apply {
+                    if (this) {
+                        also { focusManager.moveFocus(FocusDirection.Down) }
+                    }
+                }
+            },
+        value = username,
+        onValueChange = { value ->
+            username = value
+        },
+        label = "Username",
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Done,
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.moveFocus(FocusDirection.Down)
+            },
+        ),
+    )
 }
 
 @Preview(showBackground = true)
